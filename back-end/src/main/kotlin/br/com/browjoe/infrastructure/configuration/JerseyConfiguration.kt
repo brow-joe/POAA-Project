29a6bce11e4e22bf0e3e0a50c00d836e85b2edca
brow-joe <jo_souza92@yahoo.com.br>
2017-12-16 19:29:01 -0200

    configure jersey into spring-boot

---
+++ back-end/src/main/kotlin/br/com/browjoe/infrastructure/configuration/JerseyConfiguration.kt
@@ -0,0 +1,12 @@
+package br.com.browjoe.infrastructure.configuration
+
+import org.glassfish.jersey.server.ResourceConfig
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+open class JerseyConfiguration : ResourceConfig() {
+
+	init {
+	}
+
+}
