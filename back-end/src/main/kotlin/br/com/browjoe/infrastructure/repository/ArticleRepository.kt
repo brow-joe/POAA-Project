2f5ca1e5ff3877f7d7b1a6b31bc013e2668d5a68
brow-joe <jo_souza92@yahoo.com.br>
2017-12-16 20:46:46 -0200

    create findAll

--- back-end/src/main/kotlin/br/com/browjoe/infrastructure/repository/ArticleRepository.kt
+++ back-end/src/main/kotlin/br/com/browjoe/infrastructure/repository/ArticleRepository.kt
@@ -1,11 +1,18 @@
 package br.com.browjoe.infrastructure.repository
 
+import br.com.browjoe.jooq.data.tables.Article
+import br.com.browjoe.jooq.data.tables.records.ArticleRecord
 import org.jooq.DSLContext
+import org.jooq.Result
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 open class ArticleRepository(var dsl: DSLContext) {
 	val logger = LoggerFactory.getLogger(ArticleRepository::class.java);
+
+	fun findAll(): Result<ArticleRecord> {
+		return dsl.selectFrom(Article.ARTICLE).fetch();
+	}
 
 }
--- back-end/src/main/kotlin/br/com/browjoe/infrastructure/repository/PostRepository.kt
+++ back-end/src/main/kotlin/br/com/browjoe/infrastructure/repository/PostRepository.kt
@@ -1,11 +1,18 @@
 package br.com.browjoe.infrastructure.repository
 
+import br.com.browjoe.jooq.data.tables.Post
+import br.com.browjoe.jooq.data.tables.records.PostRecord
 import org.jooq.DSLContext
+import org.jooq.Result
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 open class PostRepository(var dsl: DSLContext) {
 	val logger = LoggerFactory.getLogger(PostRepository::class.java);
+
+	fun findAll(): Result<PostRecord> {
+		return dsl.selectFrom(Post.POST).fetch();
+	}
 
 }
