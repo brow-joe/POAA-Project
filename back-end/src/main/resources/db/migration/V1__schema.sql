73f93aba520586b461554a82d4d0619502a79c10
brow-joe <jo_souza92@yahoo.com.br>
2017-12-16 11:13:35 -0200

    update sql schema

--- back-end/src/main/resources/db/migration/V1__schema.sql
+++ back-end/src/main/resources/db/migration/V1__schema.sql
@@ -1,4 +1,20 @@
-CREATE TABLE poaa (
-  id BIGINT PRIMARY KEY,
-  uuid TEXT
+CREATE TABLE articles (
+  id SERIAL PRIMARY KEY,
+  wp_internal_id BIGINT NOT NULL,
+  wp_published_time TIMESTAMP NOT NULL,
+  title VARCHAR(110) NOT NULL,
+  slug VARCHAR(110) NOT NULL
+);
+
+CREATE TABLE posts(
+  id SERIAL PRIMARY KEY,
+  fb_internal_id VARCHAR(35) NOT NULL,
+  fb_published_date TIMESTAMP NOT NULL,
+  fb_title VARCHAR(120) NOT NULL,
+  fb_link VARCHAR(150) NOT NULL,
+  fb_description VARCHAR(500),
+  likes INT NOT NULL,
+  shares INT NOT NULL,
+  comments INT NOT NULL,
+  consumptions INT NOT NULL
 );
