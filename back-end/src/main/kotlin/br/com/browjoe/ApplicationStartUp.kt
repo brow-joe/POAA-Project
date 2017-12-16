de51b8d0704d45e753da7dddacdfe7c7b37ef21d
brow-joe <jo_souza92@yahoo.com.br>
2017-12-16 16:24:56 -0200

    configure main class

--- back-end/src/main/kotlin/br/com/browjoe/ApplicationStartUp.kt
+++ back-end/src/main/kotlin/br/com/browjoe/ApplicationStartUp.kt
@@ -1,7 +1,19 @@
 package br.com.browjoe
 
-class ApplicationStartUp
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.support.SpringBootServletInitializer
+
+@SpringBootApplication
+open class ApplicationStartUp : SpringBootServletInitializer() {
+
+	override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+		return application.sources(ApplicationStartUp::class.java);
+	}
+
+}
 
 fun main(args: Array<String>) {
-	println();
+	SpringApplication.run(ApplicationStartUp::class.java, *args);
 }
