17880465797d325f6958027bc1746c3b8ce3d1f5
brow-joe <jo_souza92@yahoo.com.br>
2018-01-02 20:24:27 -0200

    configure dataset in schema

--- back-end/src/main/resources/db/migration/V1__schema.sql
+++ back-end/src/main/resources/db/migration/V1__schema.sql
@@ -2,8 +2,8 @@ CREATE TABLE article (
   id SERIAL PRIMARY KEY,
   wp_internal_id BIGINT NOT NULL,
   wp_published_time TIMESTAMP NOT NULL,
-  title VARCHAR(110) NOT NULL,
-  slug VARCHAR(110) NOT NULL
+  title VARCHAR(110) NOT NULL UNIQUE,
+  slug VARCHAR(110) NOT NULL UNIQUE
 );
 
 CREATE TABLE post (
@@ -17,4 +17,18 @@ CREATE TABLE post (
   shares INT NOT NULL,
   comments INT NOT NULL,
   consumptions INT NOT NULL
+);
+
+CREATE TABLE filter (
+  id SERIAL PRIMARY KEY,
+  start_date TIMESTAMP NOT NULL,
+  end_date TIMESTAMP NOT NULL
+);
+
+CREATE TABLE article_score (
+  id SERIAL PRIMARY KEY,
+  article_id SERIAL REFERENCES article NOT NULL,
+  filter_id SERIAL REFERENCES filter NOT NULL,
+  score DOUBLE PRECISION NOT NULL,
+  grouped VARCHAR(50) NOT NULL
 );
